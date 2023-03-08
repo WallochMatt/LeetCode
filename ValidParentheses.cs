c8c0213428762ddea5e38e78b19af10c37ecd7a2
Matthew Walloch <111300455+WallochMatt@users.noreply.github.com>
2023-03-08 13:24:59 -0600

    Added Leetcode 20. Valid Parentheses

---
+++ ValidParentheses.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class ValidParentheses
+    {
+        public static bool IsValid(string s)
+        {
+            Dictionary<char, char> bracketsMap = new Dictionary<char, char>
+            {
+                {'{',  '}'},
+                {'(',  ')'},
+                {'[',  ']'},
+            };
+            Stack<char> stackOfBrackets = new Stack<char>();
+
+            foreach (char bracket in s)
+            {
+                if (bracketsMap.ContainsKey(bracket))
+                {
+                    stackOfBrackets.Push(bracket);
+                }
+                else
+                {
+                    if (stackOfBrackets.Count == 0)
+                    {
+                        return false;
+                    }
+                    if (bracketsMap[stackOfBrackets.Pop()] == bracket)
+                    {
+                        continue;
+                    };
+                    return false;
+                }
+            }
+            return stackOfBrackets.Count == 0;
+        }
+    }
+}
