a3894f133a4e9d36243496a598e04eac5dd91ca3
Matthew Walloch <111300455+WallochMatt@users.noreply.github.com>
2023-03-09 13:57:15 -0600

    LeetCode 258. Add Digits

---
+++ AddDigitsSolution.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class AddDigitsSolution
+    {
+        public static int AddDigits(int num)
+        {
+            string numAsString = num.ToString();
+            double addedDigits = 0;
+
+            for (int i = 0; i < numAsString.Length; i++)
+            {
+                addedDigits += Char.GetNumericValue(numAsString[i]);
+            }
+
+            if (addedDigits > 9)
+            {
+                addedDigits = AddDigits(Convert.ToInt32(addedDigits));
+            }  
+
+            return Convert.ToInt32(addedDigits);
+        }
+
+    }
+}
