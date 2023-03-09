4cf70449d6a93d3eb7dbbe59ce98c8c5e72578ec
Matthew Walloch <111300455+WallochMatt@users.noreply.github.com>
2023-03-09 14:18:26 -0600

    LeetCode 28
    28. Find the Index of the First Occurrence in a String

---
+++ FindFirstOccurence.cs
@@ -0,0 +1,24 @@
+﻿using LeetCode;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class FindFirstOccurence
+    {
+        public int StrStr(string haystack, string needle)
+        {
+            return haystack.IndexOf(needle);   
+        }
+
+    }
+}
+
+//FindFirstOccurence test = new FindFirstOccurence();
+//int result = test.StrStr("sadbutsad", "but");
+
+//Console.WriteLine(result);
+//Console.ReadLine();
