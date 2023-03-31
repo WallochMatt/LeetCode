5577261b0275a60a056a7663f88e0a575eed8e62
Matthew Walloch <111300455+WallochMatt@users.noreply.github.com>
2023-03-31 14:32:31 -0500

    Added Fibonacci calculator

---
+++ Fib.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    internal class Fib
+    {
+        public int fib(int n) 
+        {
+            int x = 0;
+            int y = 1; 
+            int z = 1;
+
+
+            for (int i = 0; i < n; i++)
+            {
+                z = x + y;
+                x = y;
+                y = z;
+            }
+            return z;
+        }
+    }
+}
