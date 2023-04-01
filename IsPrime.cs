eb64ddfc24dc4fe3d20155eeefc20604f6e7ed1b
Matthew Walloch <111300455+WallochMatt@users.noreply.github.com>
2023-03-31 21:35:46 -0500

    Add files via upload

---
+++ IsPrime.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+
+    internal class IsPrime
+    {
+        public bool checksPrime(int givenNumber)
+        {
+            if (givenNumber <= 1) 
+            { 
+                return false;
+            }
+
+            int currentNumber = 2;
+            while (currentNumber <= (givenNumber / 2)) 
+            { 
+                if (givenNumber % currentNumber == 0) 
+                { 
+                    return false;
+                }
+                currentNumber++;
+            }
+            return true;
+            
+        }
+
+    }
+}
